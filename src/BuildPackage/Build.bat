76bb589d86fc67bc0b4262e190367fba96d7b88c
Stefano Chiodino <stefano.chiodino@epiphanysolutions.co.uk>
2017-08-21 10:52:35 +0100

    AppVeyor build boilerplate

---
+++ src/BuildPackage/Build.bat
@@ -0,0 +1,8 @@
+ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
+ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
+ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
+ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
+
+Call Tools\nuget.exe restore ..\UmbracoAuditViewer.sln
+cd ..\BuildPackage\
+Call "%programfiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" package.proj
